f1bad23ad511d80a4f9b799d6b464b9ffd5bd080
clint <cguin@kellyway.com>
2019-06-14 16:57:59 -0400

    init

---
+++ src/popup/popup.js
@@ -0,0 +1,18 @@
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.compact.css';
+
+import Vue from 'vue';
+import App from './App';
+import store from '../store';
+import router from './router';
+
+global.browser = require('webextension-polyfill');
+Vue.prototype.$browser = global.browser;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  router,
+  render: h => h(App),
+});
---
+++ src/popup/router/pages/Index.vue
@@ -0,0 +1,37 @@
+<template>
+
+<div style="width:700px">
+  <p>fdsgfddfgdgdgdgdfgHello world!{{animatedNumber}} fdsvfg</p>
+      <dx-button :text='text'/>
+      <QueriesGrid/>
+</div>
+
+</template>
+
+<script>
+import DxButton from 'devextreme-vue/button';
+import QueriesGrid from './QueriesGrid';
+export default {
+ mounted() {
+  },
+  data() {
+    return {
+      text: "click",
+      animatedNumber: this.$store.getters.foo,
+      queries:{
+        name:"default",
+        selectors:[
+          "page"
+        ]
+      }
+    };
+  },
+  components: {
+    DxButton,
+    QueriesGrid
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
